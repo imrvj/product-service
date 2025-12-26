5d9b3eeaba5f677088c760fe93089715b37888c3
RV <sng.rv07@gmail.com>
2025-12-26 22:19:46 +0530

    if else

--- src/main/java/com/rv/product_service/Service/ProductReviewImpl.java
+++ src/main/java/com/rv/product_service/Service/ProductReviewImpl.java
@@ -8,6 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductReviewImpl implements ProductReview {
     @Override
     public ProductReviewEntity getReview(int productId) {
-        return new ProductReviewEntity(productId,"authorName","Description Some","Some Big content to Display here");
+        if (productId>5 ){
+            return new ProductReviewEntity(productId,"authorName","Description Some","Some Big content to Display here");
+        }
+        else if (productId<5)
+        {
+            return new ProductReviewEntity(productId,"RV","Description less then 5","Some Big content to Display here");
+        }
+        else
+        {
+            return new ProductReviewEntity(productId, "RV", "Description Not in Range", "Some Big content to Display here");
+        }
     }
 }

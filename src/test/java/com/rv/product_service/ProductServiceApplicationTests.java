f481b68827b10e647d85e6db96b1617fc2c477eb
RV <sng.rv07@gmail.com>
2025-12-27 11:18:29 +0530

    Test Code

--- src/test/java/com/rv/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/rv/product_service/ProductServiceApplicationTests.java
@@ -2,12 +2,48 @@ package com.rv.product_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    private WebTestClient webTestClient() {
+        return WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .build();
+    }
+
+
+    @Test
+    void get200() {
+        int PRODUCT_ID = 1;
+        webTestClient().get()
+                .uri("/product/" + PRODUCT_ID)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType("application/json")
+                .expectBody()
+                .jsonPath("$.productId").isEqualTo(1);
+
+    }
+
+    @Test
+    void get404() {
+        int PRODUCT_ID = 1;
+        webTestClient().get()
+                .uri("/product/")
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectHeader().contentType("application/json")
+                .expectBody();
+
+
+    }
 
 }

df0a56fcb79ba0b63628af1dffec0fbf4531b964
RV <sng.rv07@gmail.com>
2025-12-29 15:02:45 +0530

    add postgress and table

--- src/main/java/com/rv/product_service/Entity/ProductEntity.java
+++ src/main/java/com/rv/product_service/Entity/ProductEntity.java
@@ -1,12 +1,23 @@
 package com.rv.product_service.Entity;
 import lombok.*;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "product_entity")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductEntity {
+    @Id
+    @Column(name = "product_id")
     private int productId;
+    
+    @Column(name = "name")
     private String name;
+    
+    @Column(name = "weight")
     private int weight;
-    private String Expiry;
+    
+    @Column(name = "expiry")
+    private String expiry;
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS product_entity (
+    product_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    weight INT,
+    expiry VARCHAR(255)
+);

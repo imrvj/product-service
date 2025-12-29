00b32588e7532bb76d0de7bbddc194ebe88d3c1b
RV <sng.rv07@gmail.com>
2025-12-29 17:20:58 +0530

    checkstyle

--- src/main/java/com/rv/product_service/Controller/Product.java
+++ src/main/java/com/rv/product_service/Controller/Product.java
@@ -1,9 +1,6 @@
 package com.rv.product_service.Controller;
 
 import com.rv.product_service.Entity.ProductEntity;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +14,7 @@ public interface Product {
 
     @PatchMapping(value = "/update/{productId}", produces = "application/json")
     ProductEntity updateProduct(
-        @PathVariable int productId);
+            @PathVariable int productId);
 
     @PostMapping(value = "/insert/{productId}", produces = "application/json")
     ProductEntity insertProduct(
